0b2a32983d24770cfbc533c47fae34a855136220
michael <michael.green94@hotmail.com>
2018-10-23 18:21:08 -0600

    created item model

---
+++ Models/Item.cs
@@ -0,0 +1,11 @@
+namespace leagueapp.Models
+{
+  public class Item
+  {
+    public string name { get; set; }
+    public string description { get; set; }
+    public string img { get; set; }
+    public int cost { get; set; }
+    public string stats { get; set; }
+  }
+}
